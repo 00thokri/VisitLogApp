fc9c39ff951dd5dfbff4b2114921683ccd1505b7
00thokri <thobias2011@live.se>
2025-09-13 18:02:18 +0200

    Fixed javascript for submit button

--- Backend/HttpTrigger1.cs
+++ Backend/HttpTrigger1.cs
@@ -15,7 +15,9 @@ public class HttpTrigger1
     }
 
     [Function("HttpTrigger1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req
+    )
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,1 +1,41 @@
 //On submit button click, send post request to server with form data 
+function submitVisit() {
+    const submitButton = document.getElementById("submitbutton");
+    submitButton.addEventListener("click", () => {
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const visitor = {
+            Name: name,
+            Email: email
+        };
+        sendVisitToAPI(visitor);
+    })
+}
+
+async function sendVisitToAPI(visitor) {
+    //TESTING PURPOSES:
+    alert("Sending visit to API: " + JSON.stringify(visitor));
+    const response = await fetch("API LINK HERE", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(visitor)
+    });
+
+    if (response.ok) {
+        alert("Thank you for your visit!");
+    }
+    else {
+        const errorText = await response.text();
+        throw new Error("Failed to submit visit: " + errorText);
+    }
+    return await response.json();
+}
+
+function main()
+{
+    submitVisit();
+}
+main();
